b750e68f4831121d61bfdafbddecb9e981883b33
Cole Hart <cole@colehart.com>
2018-05-18 10:25:54 -0600

    initial setup complete. first model test for book passing title validation

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Book, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+  end
+  # describe 'relationships' do
+  #   it { should have_many(:book_users) }
+  # end
+end
